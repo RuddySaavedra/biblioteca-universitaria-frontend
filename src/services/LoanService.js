398bd3d241358972a26767283f44e4c8c126441e
Matilde <a2024112013@estudiantes.upsa.edu.bo>
2025-11-03 12:31:09 -0400

    LoanService.js

---
+++ src/services/LoanService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// URL base del controlador Loan en el backend
+const API_REST_BASE_URL = "http://localhost:8080/api/loans";
+
+// Métodos para consumir el backend
+export const getAllLoans = () => axios.get(API_REST_BASE_URL);
+export const addLoan = (loan) => axios.post(API_REST_BASE_URL, loan);
+export const deleteLoan = (id) => axios.delete(API_REST_BASE_URL + "/" + id);
+export const updateLoan = (id, loan) =>
+    axios.put(API_REST_BASE_URL + "/" + id, loan);
+export const getLoan = (id) => axios.get(API_REST_BASE_URL + "/" + id);
